1b97db166a72bcfb456dcfba55d0f0aba3b1f8cf
Andy <amarden@usa.net>
2026-07-09 13:33:06 +0100

    added thumbnail and *images to storefront products.ts

--- apps/storefront/src/lib/data/products.ts
+++ apps/storefront/src/lib/data/products.ts
@@ -70,7 +70,7 @@ export const listProducts = async ({
           offset,
           region_id: region?.id,
           fields:
-	    "id,title,handle,thumbnail,*images,*variants.calculated_price,+variants.inventory_quantity,*variants.images,*variants.options,+metadata,+tags,",
+	    "*variants.calculated_price,+variants.inventory_quantity,*variants.images,*variants.options,+metadata,+tags,*images,thumbnail,",
           ...queryParams,
         },
         headers,
