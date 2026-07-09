c9a0a7c36d43a6d34c1902489f6c6e32b00286d8
Andy <amarden@usa.net>
2026-07-09 06:56:23 +0100

    added export const dynamic = force-dynamic to country and top level layout scipts

--- apps/storefront/src/app/layout.tsx
+++ apps/storefront/src/app/layout.tsx
@@ -2,6 +2,8 @@ import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 import "styles/globals.css"
 
+export const dynamic = "force-dynamic"
+
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
